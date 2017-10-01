e31c469e299d712ed252329a97c0a3dad67197cf
An, Albert <contendera@gmail.com>
2017-10-01 15:34:00 -0700

    Added email /password input
    Added email /password input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,18 +2,28 @@ import React, {Component} from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state ={ text: ''};
+    state ={ email: '', password: ''};
 
     render(){
         return(
             <Card>
                <CardSection>
                    <Input
-                   value={this.state.text}
-                   onChangeText={ () => this.setState({ text: text})} 
+                   placeholder="example@gmail.com"
+                   label = "Email"
+                   value={this.state.email}
+                   onChangeText={ text => this.setState({ email: text })} 
                    />
                </CardSection>
-               <CardSection />
+               <CardSection >
+                   <Input
+                   secureTextEntry
+                   placeholder ="password"
+                   label="Password"
+                   value={this.state.password}
+                   onChangeText={ text => this.setState({password: text})}
+                   />
+                </CardSection>
                <CardSection>
                    <Button>
                        Log in
